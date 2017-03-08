8bd8eaa79369d042b0bd208e1631bcc52a6c934e
Irene <irene.g5555@gmail.com>
2017-03-08 00:08:25 +0000

    Fix MongoDB issue - missing _id field. Organise graphs & add some style

--- marvel.py
+++ marvel.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 DBS_NAME = 'marvel'
-COLLECTION_NAME = 'avengers'
-FIELDS = {'Name/Alias': True, 'Appearances': True, 'Current': True, 'Gender': True,
-          'Full/Reserve': True, 'Year': True, 'Honorary': True, 'Death1': True}
+COLLECTION_NAME = 'avengers-clean'
+FIELDS = {'_id': False, 'Name-Alias': True, 'Appearances': True, 'Current': True, 'Gender': True,
+          'Full-Reserve': True, 'Year': True, 'Honorary': True, 'Death1': True}
 
 
 
@@ -19,14 +19,14 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/donorsUS/projects")
+@app.route("/marvel/avengers-clean")
 def marvel_avengers():
     connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
     avengers = collection.find(projection=FIELDS, limit=55000)
     json_avengers = []
-    for avng in avengers:
-        json_avengers.append(avng)
+    for avenger in avengers:
+        json_avengers.append(avenger)
     json_avengers = json.dumps(json_avengers)
     connection.close()
     return json_avengers
--- static/js/graph.js
+++ static/js/graph.js
@@ -6,7 +6,7 @@
 //Useful to get data from multiple API's for a single analysis.
 //This function processes the data and inserts it into the apiData Variable
 queue()
-    .defer(d3.json, "static/json/marvel-avengers.json") // Get the data from MongoDB
+    .defer(d3.json, "/marvel/avengers-clean") // Get the data from MongoDB
     .await(makeGraphs);
 
 
@@ -17,7 +17,6 @@ function makeGraphs(error, avengersJson){
     avengers.forEach(function(d){
         d["Appearances"] = +d["Appearances"]
         d["Year"] = +d["Year"]
-        console.log(d["Current"])
     });
 
     //Ingesting the data into a crossfilter instance and creating dimensions based on the crossfilter instance
@@ -45,9 +44,10 @@ function makeGraphs(error, avengersJson){
     var death1Dim = ndx.dimension(function (d) {
         return d["Death1"]
     });
-    var appearancesDim = ndx.dimension(function (d) {
-        return d["Appearances"]
-    })
+
+    var avengersDim = ndx.dimension(function (d) {
+        return d["Name-Alias"]
+    });
 
     //Calculate metrics and groups for grouping and counting our data
     var yearGroup = yearDim.group();
@@ -57,18 +57,14 @@ function makeGraphs(error, avengersJson){
     var numAvengersByGender = genderDim.group();
     var numAvengersByHonorary = honoraryDim.group();
     var numAvengersByYear = yearDim.group();
-    var numAvengersByAppearances = appearancesDim.group();
-    var totalAppearancesByGender = genderDim.group().reduceSum(function (d){
-        return d["Appearances"];
-    });
 
-
+    var avengersGroup = avengersDim.group();
+    var numAvengersByDeath1 = death1Dim.group();
     var all=ndx.groupAll();
 
 
 
     //Define values (to be used in charts)
-//Define values (to be used in charts)
     var minYear = yearDim.bottom(1)[0]["Year"];
     var maxYear = yearDim.top(1)[0]["Year"];
 
@@ -76,27 +72,25 @@ function makeGraphs(error, avengersJson){
     //We also bind the charts to the div ID's in index.html
     //Charts
     var yearChart = dc.barChart("#year-bar-chart");
-    var appearancesChart = dc.barChart("#appearances-bar-chart");
-    var honoraryRowChart = dc.rowChart("#honorary-row-chart");
     var honoraryChart = dc.pieChart("#honorary-pie-chart");
     var genderChart = dc.pieChart("#gender-pie-chart");
     var numberAvengers = dc.numberDisplay("#number-avengers");
-    //var currentChart = dc.pieChart("#current-chart");
+    var currentChart = dc.pieChart("#current-chart");
+    var death1RowChart = dc.rowChart("#death1-row-chart")
 
 
 
-
-        //We assign properties and values to our charts.
+    //We assign properties and values to our charts.
     //We also include a select menu to choose between any of all US states for a particular date
- selectField = dc.selectMenu('#menu-select-current')
+    selectField = dc.selectMenu('#menu-select-current')
         .dimension(currentDim)
         .group(currentGroup);
 
-    // Select State Menu. When used it overwrites the US map
-   /* selectField = dc.selectMenu('#menu-select')
-        .dimension(genderDim)
-        .group(genderGroup);
-*/
+
+    selectFieldName = dc.selectMenu('#menu-select-avenger')
+     .dimension(avengersDim)
+     .group(avengersGroup);
+
     // Metric Num Avengers
     numberAvengers
         .formatNumber(d3.format("d"))
@@ -105,51 +99,10 @@ function makeGraphs(error, avengersJson){
         })
         .group(all);
 
-   // Pie Chart Honorary
-    honoraryChart
-        .height(220)
-        .radius(90)
-        .innerRadius(40)
-        .transitionDuration(1500)
-        .dimension(honoraryDim)
-        .group(numAvengersByHonorary)
-       // .colors(d3.scale.ordinal().range([ '#85C1E9', '#6E2C00', '#52BE80']))
-        ;
-
-     // Pie Chart Gender
-    genderChart
-        .height(220)
-        .radius(90)
-        .innerRadius(0)
-        .transitionDuration(1500)
-        .dimension(genderDim)
-        .group(numAvengersByGender)
-        ;
-
-    // Row Chart Resource Type
-    honoraryRowChart
-        .width(300)
-        .height(250)
-        .dimension(honoraryDim)
-        .group(numAvengersByHonorary)
-        //.colors(d3.scale.ordinal().range([ '#000000', '#D4AC0D', '#6E2C00']))
-        .xAxis().ticks(4);
-
-     // Pie Chart Current
-    /*currentChart
-        .height(220)
-        .radius(90)
-        .innerRadius(20)
-        .transitionDuration(1500)
-        .dimension(currentDim)
-        .group(numAvengersByCurrent)
-       // .colors(d3.scale.ordinal().range([ '#85C1E9', '#6E2C00', '#52BE80']))
-        ;*/
-
     //Time-line in Years
     yearChart
         .width(800)
-        .height(200)
+        .height(220)
         .margins({top:20, right:50, bottom:30, left:50})
         .dimension(yearDim)
         .group(numAvengersByYear)
@@ -159,26 +112,61 @@ function makeGraphs(error, avengersJson){
         .xAxisLabel("Year")
         .yAxis().ticks(8);
 
+    // Pie Chart Current
+    currentChart
+        .height(220)
+        .radius(90)
+        .innerRadius(20)
+        .transitionDuration(1500)
+        .dimension(currentDim)
+        .group(numAvengersByCurrent)
+        .colors(d3.scale.ordinal().range(['#FF8000', '#3399FF', '#66CC00', '#FFFF33']))
+        ;
 
 
-    //Bar Chart Appearances
-    appearancesChart
-        .width(600)
-        .height(200)
-        .margins({top:10, right:10, bottom:50, left:50})
-        .dimension(appearancesDim)
-        .group(numAvengersByAppearances)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal().domain(1, 4050))
-        .y(d3.scale.linear().domain([0,20]))
-        .xUnits(dc.units.ordinal)// Tell dc.js that we're using an ordinal x-axis;
-        .elasticY(true)
-        .xAxisLabel("Appearances")
-        .gap(10)
-        .yAxis().ticks(3);
+   // Pie Chart Honorary
+    honoraryChart
+        .height(220)
+        .radius(90)
+        .width(300)
+        .innerRadius(40)
+        .dimension(honoraryDim)
+        .group(numAvengersByHonorary)
+        .colors(d3.scale.ordinal().range(['#FF8000', '#3399FF', '#66CC00', '#FFFF33']))
+        .legend(dc.legend().x(0).y(10))
+        .minAngleForLabel(0.5)
+        .externalLabels(0.05)
+        .slicesCap(5)
+        .renderLabel(true)
+        .transitionDuration(500);
+
+
+     // Pie Chart Gender
+    genderChart
+        .height(220)
+        .radius(90)
+        .innerRadius(0)
+        .transitionDuration(1500)
+        .dimension(genderDim)
+        .group(numAvengersByGender)
+       .colors(d3.scale.ordinal().range(['#FF8000', '#3399FF', '#66CC00', '#FFFF33']));
+
+
+    // Row Chart Death1
+    death1RowChart
+        .width(300)
+        .height(220)
+        .dimension(death1Dim)
+        .group(numAvengersByDeath1)
+        .colors(d3.scale.ordinal().range([ '#FF8000', '#3399FF', '#66CC00', '#FFFF33']))
+        .xAxis().ticks(4);
 
 
 
 
-     dc.renderAll();
+
+
+
+
+    dc.renderAll();
 }
